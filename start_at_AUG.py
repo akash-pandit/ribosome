471b8f3b9450daeb80000eb4fe5a1e8409337e55
Akash Pandit <34327539+akash-pandit@users.noreply.github.com>
2023-02-25 18:34:45 -0800

    Add files via upload

---
+++ start_at_AUG.py
@@ -0,0 +1,52 @@
+from re import search, findall
+
+codons = {
+    "UUU": "Phe", "UCU": "Ser", "UAU": "Tyr", "UGU": "Cys",
+    "UUC": "Phe", "UCC": "Ser", "UAC": "Tyr", "UGC": "Cys",
+    "UUA": "Leu", "UCA": "Ser", "UAA": False, "UGA": False,
+    "UUG": "Leu", "UCG": "Ser", "UAG": False, "UGG": "Trp",
+
+    "CUU": "Leu", "CCU": "Pro", "CAU": "His", "CGU": "Arg",
+    "CUC": "Leu", "CCC": "Pro", "CAC": "His", "CGC": "Arg",
+    "CUA": "Leu", "CCA": "Pro", "CAA": "Gln", "CGA": "Arg",
+    "CUG": "Leu", "CCG": "Pro", "CAG": "Gln", "CGG": "Arg",
+
+    "AUU": "Ile", "ACU": "Thr", "AAU": "Asn", "AGU": "Ser",
+    "AUC": "Ile", "ACC": "Thr", "AAC": "Asn", "AGC": "Ser",
+    "AUA": "Ile", "ACA": "Thr", "AAA": "Lys", "AGA": "Arg",
+    "AUG": "Met", "ACG": "Thr", "AAG": "Lys", "AGG": "Arg",
+
+    "GUU": "Val", "GCU": "Ala", "GAU": "Asp", "GGU": "Gly",
+    "GUC": "Val", "GCC": "Ala", "GAC": "Asp", "GGC": "Gly",
+    "GUA": "Val", "GCA": "Ala", "GAA": "Glu", "GGA": "Gly",
+    "GUG": "Val", "GCG": "Ala", "GAG": "Glu", "GGG": "Gly"
+}
+
+mRNA = input()
+while mRNA.strip() != findall('[ACUG]+', mRNA)[0]:
+    print('Invalid input, please provide a string solely composed of the characters A, G, C, or U as seen in mRNA.')
+    mRNA = input()
+
+mRNA = search('AUG\w+', mRNA).group()
+
+mRNA = [mRNA[i:i+3] for i in range(0, len(mRNA), 3)]
+
+polypeptides, polypeptide = [], []
+terminated = True
+for i in range(len(mRNA)):
+    if mRNA[i] == 'AUG' and terminated:
+        terminated = False
+        polypeptide.append('Met')
+    elif mRNA[i] is mRNA[-1] and codons[mRNA[i]]:
+        polypeptides.append(polypeptide.copy())
+        print('ALERT: The above polypeptide had no stop sequence, and terminated due to the end of the mRNA sequence.')
+    elif polypeptide:
+        if codons[mRNA[i]]:
+            polypeptide.append(codons[mRNA[i]])
+        else:
+            terminated = True
+            polypeptides.append(polypeptide.copy())
+            polypeptide.clear()
+
+for i in polypeptides:
+    print('-'.join(i))
